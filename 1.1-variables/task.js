b04aba149c60c371524ff343928ae198c5367d6f
ReactORcss <57535924+ReactORcss@users.noreply.github.com>
2020-08-18 17:56:07 +0300

    Update task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,25 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
-function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
-}
+ function averageMark(a, g, p) {
 
-function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
-}
+     return (a + g + p) / 3;
+ }
 
-function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
-}
+ function sayHello(userName) {
+
+     let myName = userName;
+     let message = `Привет, мир! Меня зовут ${myName}`;
+
+     return message;
+ }
+
+ function calculateFormula() {
+
+     let x = 2,
+         y = 22,
+         z = 0;
+     let result = ((x * y) + (5 * z) + x - 1);
+
+     return result;
+ }
