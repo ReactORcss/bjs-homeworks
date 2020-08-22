e1b8159fac8d7b1a4ed99434c07b249030666077
Sorokin Alexey <alexwsop@gmail.com>
2020-08-22 15:16:49 +0300

    First commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,36 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    percent = parseInt(percent);
+    contribution = parseInt(contribution);
+    amount = parseInt(amount);
+    if (isNaN(percent)) {
+        return `Параметр percent содержит неправильное значение ${percent}`;
+    }
+    if (isNaN(contribution)) {
+        return `Параметр contribution содержит неправильное значение ${contribution}`;
+    }
+    if (isNaN(amount)) {
+        return `Параметр amount содержит неправильное значение ${amount}`;
+    }
+    if (isNaN(date)) {
+        return `Параметр date содержит неправильное значение ${date}`;
+    }
+    let P = (percent / 100) / 12;
+    let currentAgeInMs = Date.now(); // текущая дата в мс
+    date = Math.floor((date - currentAgeInMs) / 2592000000); // делим на кол-во мс в месяце и округляем до целого
+    // let today = new Date();
+    // let n = (date.getFullYear() - today.getFullYear()) * 12 + date.getMonth() - today.getMonth();
+    let monthAmount = (amount - contribution) * (P + P / ((Math.pow(1 + P, date)) - 1));
+    let totalAmount = monthAmount * date;
+    console.log(+totalAmount.toFixed(2));
+    return +totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    if (name == undefined || name == null || name == '') {
+        return `Привет, мир! Меня зовут Аноним`;
+    }
+    return greeting;
 }
