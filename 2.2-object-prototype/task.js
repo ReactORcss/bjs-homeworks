dc20ac2fe4a17cc2854a74b4dbaa1067d4344cf4
Sorokin Alexey <alexwsop@gmail.com>
2020-08-30 13:45:23 +0300

    First commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,55 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    console.log(this);
+    let newstr;
+    let newstrarr;
+    let palindrome;
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (typeof this !== "number") {
+        newstr = this.toLowerCase();
+    } else {
+        newstr = this.toString();
+    }
+
+    newstrarr = newstr.split('');
+    newstr = newstr.replace(/\s/g, '');
+
+    newstrarr.reverse();
+
+    palindrome = newstrarr.join('').replace(/\s/g, '');
+
+    if (+palindrome !== NaN) {
+        +palindrome;
+    }
+
+    if (palindrome === newstr) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+// Задача № 2
+function getAverageMark(marks) {
+    let sum = 0;
+    if (marks.length == 0) {
+        return 0;
+    }
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    return Math.round(sum / marks.length);
+}
+
+
+// Задача № 3
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now(); //текущая дата в мс
+    birthday = Date.parse(birthday); // дата рождения в мс
+    let diff = now - birthday; // разница в мс
+    let age = diff / 31557600000; // делим разницу на кол-во мс в году
+    if (age > 18) {
+        return true;
+    } else {
+        return false;
+    }
 }
